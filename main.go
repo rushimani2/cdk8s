95e4cf6122d73b5e9a1e8066bc5a686d1080f645
Rushimanikanata Lingam <158359799+rushimani2@users.noreply.github.com>
2025-05-07 03:56:12 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,45 +1,35 @@
 package main
 
 import (
-	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/aws/jsii-runtime-go"
-	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
+    "github.com/aws/constructs-go/constructs/v10"
+    "github.com/aws/jsii-runtime-go"
+    "github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
 )
 
 func main() {
-	app := cdk8s.NewApp(nil)
+    app := cdk8s.NewApp(nil)
 
-	NewDeploymentChart(app, "deployment")
-	NewServiceChart(app, "service")
+    NewDeploymentChart(app, "deployment")
+    NewServiceChart(app, "service")
 
-	app.Synth()
+    app.Synth()
 }
 
 func NewDeploymentChart(scope constructs.Construct, id string) cdk8s.Chart {
-	chart := cdk8s.NewChart(scope, jsii.String(id), nil)
-
-	NewDeployment(chart,
-		jsii.String("go-web-app"),
-		&AppProps{
-			Image:         jsii.String("rushi/go-web-demo"),
-			Replicas:      jsii.Number(3),
-			Port:          jsii.Number(8080),
-			ContainerPort: jsii.Number(8080),
-		})
-
-	return chart
+    chart := cdk8s.NewChart(scope, jsii.String(id), nil)
+    NewDeployment(chart, jsii.String("go-web-app"), &AppProps{
+        Image:         jsii.String("rushi/go-web-demo"),
+        Replicas:      jsii.Number(3),
+        ContainerPort: jsii.Number(8080),
+    })
+    return chart
 }
 
 func NewServiceChart(scope constructs.Construct, id string) cdk8s.Chart {
-	chart := cdk8s.NewChart(scope, jsii.String(id), nil)
-
-	NewService(chart,
-		jsii.String("go-web-app"),
-		&AppProps{
-			Port:          jsii.Number(8080),
-			ContainerPort: jsii.Number(8080),
-		})
-
-	return chart
+    chart := cdk8s.NewChart(scope, jsii.String(id), nil)
+    NewService(chart, jsii.String("go-web-app"), &AppProps{
+        Port:          jsii.Number(8080),
+        ContainerPort: jsii.Number(8080),
+    })
+    return chart
 }
-
